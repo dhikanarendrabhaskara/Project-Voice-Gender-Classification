b513a6244d809ac800c23d08cefa94251fb657e3
dhikanarendrabhaskara <58926641+dhikanarendrabhaskara@users.noreply.github.com>
2020-04-14 22:13:31 +0700

    Add files via upload

---
+++ tpot_VOICE_GENDER.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=None)
+
+# Average CV score on the training set was: 0.9825224795098274
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="gini", max_depth=4, min_samples_leaf=12, min_samples_split=17)),
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.5, max_depth=5, max_features=0.8, min_samples_leaf=17, min_samples_split=16, n_estimators=100, subsample=0.9500000000000001)),
+    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.001, max_depth=1, max_features=1.0, min_samples_leaf=2, min_samples_split=4, n_estimators=100, subsample=0.45)),
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.1, max_depth=8, min_child_weight=12, n_estimators=100, nthread=1, subsample=0.9000000000000001)),
+    MaxAbsScaler(),
+    ExtraTreesClassifier(bootstrap=True, criterion="gini", max_features=0.7000000000000001, min_samples_leaf=18, min_samples_split=2, n_estimators=100)
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
